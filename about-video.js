5afe7c3225d09fe0fb1aef17912e7cb807cfb621
Maximiliano <br33m4rk@gmail.com>
2026-06-12 11:32:19 +0700

    Feature: Many videos play on loop

---
+++ about-video.js
@@ -0,0 +1,24 @@
+(function () {
+  var videos = [
+    "https://ik.imagekit.io/maxontorres/motovlog-feb-24.mp4",
+    "https://ik.imagekit.io/maxontorres/sanjiang-motovlog.mp4?updatedAt=1781236506482"
+  ];
+
+  var video = document.querySelector(".about-video-background video");
+  if (!video || videos.length === 0) return;
+
+  var index = 0;
+
+  function playCurrent() {
+    video.src = videos[index];
+    video.load();
+    video.play().catch(function () {});
+  }
+
+  video.addEventListener("ended", function () {
+    index = (index + 1) % videos.length;
+    playCurrent();
+  });
+
+  playCurrent();
+})();
